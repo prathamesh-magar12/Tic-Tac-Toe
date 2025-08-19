887a91a9c11667befafbc3a427628e9a9b9928fd
prathamesh-magar12 <prathamesh.magar12@gmail.com>
2025-08-19 22:17:33 +0530

    winner status is added in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from "react";
 import Square from "./components/Square";
 
 const App = () => {
   const [squares, setsquares] = useState(Array(9).fill(null));
+  const [next, setnext] = useState(true);
 
   const handleClick = (i) => {
-    if (squares[i]) return; // Ignore if square is already filled
+    if (squares[i] || calculateWinner(squares)) return; // Ignore if square is already filled
     const newSquares = squares.slice();
-    newSquares[i] = newSquares[i] === "X" ? "O" : "X";
+    newSquares[i] = next ? "X" : "O";
     setsquares(newSquares);
+    setnext(!next);
   };
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (next ? "X" : "O");
+  }
+
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -30,6 +41,26 @@ const App = () => {
       </div>
     </>
   );
+};
+
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 };
 
 export default App;
